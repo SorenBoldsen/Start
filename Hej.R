211365891c288b1a90843302de45b1f161769d64
SorenBoldsen <skbo@seges.dk>
2021-12-16 08:20:58 +0100

    Create Hej.R

---
+++ Hej.R
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+#
+# Afprøvning:  GitHub  
+# Beskrivelse:  Indlæsning af typedata
+Input=         fil # typedata fra excelfil:  
+# Output:        raadata til senere fletning 
+# Udviklet:      2021-12-16 af Søren Boldsen
+# 
+# Senest opdateret:
+#   dd/mm/yyyy  Navn  tekst
+#
+#-------------------------------------------------------------
